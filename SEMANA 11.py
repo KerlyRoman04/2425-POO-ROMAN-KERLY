a5f5104da67699f90f3f9830afa984399584f604
KERLY <km.romanp@uea.edu.ec>
2025-03-01 13:28:54 -0500

    TAREA SEMANA 8

---
+++ SEMANA 11.py
@@ -0,0 +1,31 @@
+class Producto:
+    def __init__(self, id_producto, nombre, cantidad, precio):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+
+    # Métodos getter y setter
+    def get_id(self):
+        return self.id_producto
+
+    def set_id(self, id_producto):
+        self.id_producto = id_producto
+
+    def get_nombre(self):
+        return self.nombre
+
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+
+    def get_cantidad(self):
+        return self.cantidad
+
+    def set_cantidad(self, cantidad):
+        self.cantidad = cantidad
+
+    def get_precio(self):
+        return self.precio
+
+    def set_precio(self, precio):
+        self.precio = precio
